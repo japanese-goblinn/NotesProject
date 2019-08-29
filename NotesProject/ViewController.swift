e893a71bed7055b27c46c86f058285520064a941
saisuicied <cool45akol@gmail.com>
2019-08-29 19:45:18 +0300

    Initial Commit

---
+++ NotesProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesProject
+//
+//  Created by Kirill Gorbachyonok on 8/29/19.
+//  Copyright © 2019 saisuicied. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

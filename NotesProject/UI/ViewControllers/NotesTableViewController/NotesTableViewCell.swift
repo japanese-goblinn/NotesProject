c1c4d2ebcf01eabbce179d1a59ef2e3aa560f999
japanese-goblinn <cool45akol@gmail.com>
2020-08-31 23:21:58 +0300

    feat: implemented notes VC in code

--- NotesProject/UI/ViewControllers/NotesTableViewController/NotesTableViewCell.swift
+++ NotesProject/UI/ViewControllers/NotesTableViewController/NotesTableViewCell.swift
@@ -9,7 +9,52 @@
 import UIKit
 
 class NotesTableViewCell: UITableViewCell {
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var contentLabel: UILabel!
-    @IBOutlet weak var colorView: PaletteView!
+    
+    let titleLabel = UILabel()
+    let contentLabel = UILabel()
+    let colorView = PaletteView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+     
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentLabel.translatesAutoresizingMaskIntoConstraints = false
+        colorView.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(contentLabel)
+        contentView.addSubview(colorView)
+        
+        titleLabel.numberOfLines = 1
+        titleLabel.lineBreakMode = .byTruncatingTail
+        titleLabel.font = .boldSystemFont(ofSize: 18)
+        
+        contentLabel.numberOfLines = 3
+        contentLabel.textColor = .darkGray
+        
+        
+        NSLayoutConstraint.activate([
+            colorView.heightAnchor.constraint(equalTo: titleLabel.heightAnchor),
+            colorView.widthAnchor.constraint(equalTo: colorView.heightAnchor),
+            
+            colorView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            colorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            colorView.bottomAnchor.constraint(equalTo: contentLabel.topAnchor, constant: -10),
+            
+            titleLabel.topAnchor.constraint(equalTo: colorView.topAnchor),
+            titleLabel.firstBaselineAnchor.constraint(equalTo: colorView.bottomAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: colorView.trailingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            
+            contentLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            contentLabel.leadingAnchor.constraint(equalTo: colorView.leadingAnchor),
+            contentLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor)
+        ])
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Not implemeted")
+    }
+    
 }
--- NotesProject/UI/ViewControllers/NotesTableViewController/NotesTableViewController.swift
+++ NotesProject/UI/ViewControllers/NotesTableViewController/NotesTableViewController.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 class NotesTableViewController: UIViewController {
-    
-    @IBOutlet var tableView: UITableView!
+
+    let tableView = UITableView()
     
     let notebook = FileNotebook()
     var selectedNote: Note?
     
     private func addSomeNotes() {
         let notes = [
-            Note(title: "Title", content: "Some content", date: nil),
-            Note(title: "Like really deep", content: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.", date: nil, uid: "some string", color: .yellow, priority: .important)
+            Note(title: "Title", content: "Some content", date: nil, color: .red),
+            Note(title: "Like really deep and more deep and deep and deep", content: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.", date: nil, uid: "some string", color: .yellow, priority: .important)
         ]
         for note in notes {
             notebook.add(note)
@@ -26,27 +26,29 @@ class NotesTableViewController: UIViewController {
     }
     
     @IBAction func addNote(sender: UIBarButtonItem) {
-        performSegue(withIdentifier: "showNewNote", sender: sender)
+        fatalError("Not implemented")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        tableView.register(
+            NotesTableViewCell.self,
+            forCellReuseIdentifier: String(describing: NotesTableViewCell.self)
+        )
+        tableView.tableFooterView = UIView()
         addSomeNotes()
     }
         
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        super.prepare(for: segue, sender: sender)
-        if let controller = segue.destination as? NotesDetailViewController,
-            segue.identifier == "showNote" {
-                controller.note = selectedNote
-        }
-        if let controller = segue.destination as? NotesDetailViewController,
-            segue.identifier == "showNewNote" {
-            controller.title = "New Note"
-            controller.delegate = self
-        }
-    }
-
 }
 
 extension NotesTableViewController: Noteable {
@@ -62,7 +64,10 @@ extension NotesTableViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "noteCell", for: indexPath) as! NotesTableViewCell
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: String(describing: NotesTableViewCell.self),
+            for: indexPath
+        ) as! NotesTableViewCell
         let note = notebook.notes[indexPath.row]
         cell.selectionStyle = .none
         cell.titleLabel.text = note.title
